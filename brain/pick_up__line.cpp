ddb41cd603075b64b2fcf19994de70e71986b012
Josh Brown <brownbear1002@gmail.com>
2017-01-10 16:37:26 -0500

    Update pick_up__line.cpp

--- brain/pick_up__line.cpp
+++ brain/pick_up__line.cpp
@@ -3,8 +3,8 @@ unsing namespace std;
 string void pick_up_line(){
   
   
-  const char arrayNum[4] = {'how you doing', '', '7', ''};
-  int RandIndex = rand() % 4; //generates a random number between 0 and 3
+  const char arrayNum[6] = {'how you doing', 'Mr Steal your girl', 'hey baby are you a battery cause you make me move', 'How do you get the woombas', 'show your cad files', 'hey baby are you the compiler because im the code', };
+  int RandIndex = rand() % 6; //generates a random number between 0 and 3
   cout << arrayNum[RandIndex];
   
   
