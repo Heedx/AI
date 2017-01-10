69ae78dc0e1cfde8776cc3cc9afecae68eb73baf
Josh Brown <brownbear1002@gmail.com>
2017-01-10 16:23:22 -0500

    Create pick_up__line.cpp

---
+++ brain/pick_up__line.cpp
@@ -0,0 +1,11 @@
+unsing namespace std;
+
+string void pick_up_line(){
+  
+  
+  const char arrayNum[4] = {'how you doing', '', '7', ''};
+  int RandIndex = rand() % 4; //generates a random number between 0 and 3
+  cout << arrayNum[RandIndex];
+  
+  
+}
